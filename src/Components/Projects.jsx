f00c539cbb63c9c4f3efdbace0202f3abbbe8689
Mrinal26 <Mrinalkumar5046@gmail.com>
2023-12-30 17:30:33 +0530

    cors

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -154,9 +154,9 @@ export const Projects = () => {
                   <p className="b-text project-description">
                     A platform is designed with user convenience in mind,
                     ensuring a seamless shopping experience for customers
-                    {/* looking to upgrade their tech game. Built using cutting-edge
+                    looking to upgrade their tech game. Built using cutting-edge
                     technologies such as HTML, CSS, and JavaScript, SAW-
-                    Electronics is your ultimate destination for high-quality electronics. */}
+                    Electronics is your ultimate destination for high-quality electronics.
                   </p>
                   <h2>Tech Stack</h2>
                   <div className="d-flex flex-wrap gap-2">
